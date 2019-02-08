a2ec97f4e6247cea25f7fba4217eb2f8f9b6d561
Marcelo Moraes <marcelomoraes@MacBook-Pro-de-Marcelo.local>
2019-02-08 07:25:38 -0200

    Remove static root

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -121,8 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 # Django rest configuration
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
