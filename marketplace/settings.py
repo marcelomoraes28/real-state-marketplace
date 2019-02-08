ff7bb8eb5d4cdba5fd2184e27df479f5fb0d8efd
Marcelo Moraes <marcelomoraes@MacBook-Pro-de-Marcelo.local>
2019-02-08 07:16:06 -0200

    Config static files

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -123,6 +123,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Django rest configuration
 REST_FRAMEWORK = {
